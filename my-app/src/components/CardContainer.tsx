19f6fb9293b3c25c975fdcdf7cda0d79ca3ed1b6
jiwonMe <park@jiwon.me>
2021-10-02 13:04:13 +0900

    fix

--- my-app/src/components/CardContainer.tsx
+++ my-app/src/components/CardContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { CardModel } from '../types/model'
 import Card from './Card'
 import CardFactory from './CardFactory'
